c37a317ee3128af5b97127eef49dceaea912f489
Marshall Alex <r.marshall.alex@gmail.com>
2017-08-04 16:39:51 -0500

    added register html

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { RegisterComponent } from './register/register.component'
 
 @Component({
   selector: 'app-login',
@@ -8,8 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   user = {};
   loginErrors=[];
+  selectedOption: string;
+  active_register:boolean = false;
 
-  constructor() { }
+  constructor(public dialog: MdDialog) { }
+
+  openDialog() {
+    if(!this.active_register){
+      let dialogRef = this.dialog.open(RegisterComponent);
+      dialogRef.afterClosed().subscribe(result => {
+        this.selectedOption = result;
+      });
+      this.active_register = true;
+    }
+    console.log("help me");
+  }
+
 
   ngOnInit() {
   }
